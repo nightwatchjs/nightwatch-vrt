ec374383ac3ce9ce3b6291920a2f582f8871121c
Denys Lins <denyslins@gmail.com>
2023-08-11 11:39:06 -0300

    test: add more tests

--- tests/lib/compare-with-baseline-test.js
+++ tests/lib/compare-with-baseline-test.js
@@ -8,6 +8,7 @@ const overrideBaseline = require('../../lib/override-baseline');
 const cleanupScreenshots = require('../../lib/cleanup-screenshots');
 const reporter = require('../../lib/reporter');
 const Jimp = require('jimp');
+const {assert} = require('nightwatch');
 
 jest.mock('../../lib/get-vrt-settings');
 jest.mock('../../lib/get-baseline-screenshot-or-create');
@@ -32,7 +33,8 @@ describe('compareWithBaseline', () => {
   beforeEach(() => {
     nightwatchClient = {
       capabilities: {browserName: 'chrome'},
-      currentTest: {module: 'testModule', name: 'testName'}
+      currentTest: {module: 'testModule', name: 'testName'},
+      assert: {ok: jest.fn()}
     };
     screenshot = {};
     fileName = 'testFileName';
@@ -94,5 +96,78 @@ describe('compareWithBaseline', () => {
       diff: 0.5
     });
     expect(result).toBe(false);
+  });
+
+  it('should prompt user when screenshots are non-identical and prompt is true', async () => {
+    getVrtSettings.mockReturnValue({
+      threshold: 0.1,
+      prompt: true,
+      updateScreenshots: false
+    });
+    Jimp.diff.mockReturnValue({percent: 0.5});
+    promptScreenshotOverride.mockResolvedValue(true);
+
+    const result = await compareWithBaseline(
+      nightwatchClient,
+      screenshot,
+      fileName,
+      overrideSettings,
+      context
+    );
+
+    expect(saveScreenshot).toHaveBeenCalled();
+    expect(promptScreenshotOverride).toHaveBeenCalled();
+    expect(overrideBaseline).toHaveBeenCalled();
+    expect(cleanupScreenshots).toHaveBeenCalled();
+    expect(reporter.insertData).toHaveBeenCalled();
+
+    expect(result).toBeUndefined();
+  });
+
+  it('should update baseline when screenshots are non-identical and updateScreenshots is true', async () => {
+    getVrtSettings.mockReturnValue({
+      threshold: 0.1,
+      prompt: false,
+      updateScreenshots: true
+    });
+    Jimp.diff.mockReturnValue({percent: 0.5});
+
+    const result = await compareWithBaseline(
+      nightwatchClient,
+      screenshot,
+      fileName,
+      overrideSettings,
+      context
+    );
+
+    expect(saveScreenshot).toHaveBeenCalled();
+    expect(result).toBe(true);
+  });
+
+  it('should handle errors when saving screenshot', async () => {
+    getVrtSettings.mockReturnValue({
+      threshold: 0.1,
+      prompt: false,
+      updateScreenshots: false
+    });
+    Jimp.diff.mockReturnValue({percent: 0.5});
+    saveScreenshot.mockRejectedValue(new Error('Error saving screenshot'));
+
+    const result = await expect(
+      compareWithBaseline(
+        nightwatchClient,
+        screenshot,
+        fileName,
+        overrideSettings,
+        context
+      )
+    );
+
+    expect(result).toBeInstanceOf(Object);
+    expect(saveScreenshot).not.toHaveBeenCalled();
+    expect(promptScreenshotOverride).not.toHaveBeenCalled();
+    expect(overrideBaseline).not.toHaveBeenCalled();
+    expect(cleanupScreenshots).not.toHaveBeenCalled();
+    expect(reporter.insertData).not.toHaveBeenCalled();
   });
 });
